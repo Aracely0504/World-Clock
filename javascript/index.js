2d86e61b9fad61fbe91dded42860aa8466e644b6
Aracely0504 <aracelybc90@gmail.com>
2024-09-29 23:30:15 -0700

    Add link to All Cities

--- javascript/index.js
+++ javascript/index.js
@@ -42,7 +42,9 @@ function updateCity(event) {
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small>
           </div>
+        
         </div> 
+          <a href="/"> All cities </a>
   
 `;
 }
